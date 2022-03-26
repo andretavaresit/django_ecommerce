854936a9627b8ca9fa8c4a63df8b7a500300d253
andretavaresit <andre@itpower.com.br>
2022-03-26 14:46:54 -0300

    slug_field

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,24 +1,35 @@
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+from products.views import (ProductListView, 
+                            product_list_view, 
+                            ProductDetailView, 
+                            product_detail_view,
+                            ProductDetailSlugView,
+                            ProductFeaturedListView,
+                            ProductFeaturedDetailView)
+
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
-    path('', home_page),
-    path('about/', about_page),
-    path('contact/', contact_page),
-    path('login/', login_page),
-    path('register/', register_page),
-    path('products/', ProductListView.as_view()),
-    path('products-fbv/', product_list_view), 
-    path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view), 
-    path('admin/', admin.site.urls),
-]
+	path('', home_page),
+	path('about/', about_page),
+	path('contact/', contact_page),
+        path('login/', login_page),
+        path('register/', register_page),
+        path('featured/', ProductFeaturedListView.as_view()),
+        path('featured/', ProductDetailView.as_view()),
+        path('products/', ProductListView.as_view()),
+        path('products-fbv/', product_list_view), 
+        path('products/', ProductDetailView.as_view()),
+        path('products-fbv/', product_detail_view), 
+        path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
+        path('admin/', admin.site.urls),
+        ]
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
